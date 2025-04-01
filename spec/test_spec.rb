5849988fab5a36b0c5200237d7aea04b2f6e06e9
James Couball <jcouball@yahoo.com>
2025-04-01 12:11:12 -0700

    Add test to test the result of Process.spawn chdir with an invalid directory

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,4 +1,5 @@
 require 'English'
+require 'tmpdir'
 
 describe 'Process#wait' do
   it 'should set the global $CHILD_STATUS variable' do
@@ -15,5 +16,25 @@ describe 'Process#wait2' do
     exited_pid, status = Process.wait2(pid)
     expect(status).not_to be_nil
     expect(status.pid).to eq(pid)
+  end
+end
+
+describe 'Process.spawn chdir: option' do
+  let(:script_path) { File.expand_path('bin/command-line-test') }
+
+  context 'with an invalid directory' do
+    around do |example|
+      Dir.mktmpdir do |dir|
+        Dir.chdir(dir) do
+          example.run
+        end
+      end
+    end
+
+    it 'raises an error' do
+      expect {
+        Process.spawn('ruby', script_path, chdir: 'invalid/directory')
+      }.to raise_error(Errno::ENOENT)
+    end
   end
 end
