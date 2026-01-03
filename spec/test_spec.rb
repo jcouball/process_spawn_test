5577e605e79dd52fe669a016afbf71faae8364a6
James Couball <jcouball@yahoo.com>
2026-01-02 16:00:39 -0800

    Just test that Process.wait2 returns a status and that Process.wait sets $CHILD_STATUS

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -19,66 +19,66 @@ RSpec.describe 'Process#wait2' do
   end
 end
 
-RSpec.describe 'Process.spawn with a single command argument' do
-  it 'should spawn a process via the system shell' do
-    pid = Process.spawn('exit 0')
-    _pid, status = Process.wait2(pid)
-    expect(status).not_to be_nil
-    expect(status.exitstatus).to eq(0)
-  end
-end
+# RSpec.describe 'Process.spawn with a single command argument' do
+#   it 'should spawn a process via the system shell' do
+#     pid = Process.spawn('exit 0')
+#     _pid, status = Process.wait2(pid)
+#     expect(status).not_to be_nil
+#     expect(status.exitstatus).to eq(0)
+#   end
+# end
 
-RSpec.describe 'Process.spawn chdir: option' do
-  # Execute tests in a temporary directory
-  around do |example|
-    Dir.mktmpdir do |dir|
-      Dir.chdir(dir) do
-        example.run
-      end
-    end
-  end
+# RSpec.describe 'Process.spawn chdir: option' do
+#   # Execute tests in a temporary directory
+#   around do |example|
+#     Dir.mktmpdir do |dir|
+#       Dir.chdir(dir) do
+#         example.run
+#       end
+#     end
+#   end
 
-  # The value of subject will be a Process::Status
-  subject do
-    pid = Process.spawn('ruby', script_path, chdir: chdir_path)
-    _pid, status = Process.wait2(pid)
-    status
-  end
+#   # The value of subject will be a Process::Status
+#   subject do
+#     pid = Process.spawn('ruby', script_path, chdir: chdir_path)
+#     _pid, status = Process.wait2(pid)
+#     status
+#   end
 
-  # Since we chdir in the test, make sure the path to the script is absolute
-  let(:script_path) { File.expand_path('../bin/command-line-test', __dir__) }
+#   # Since we chdir in the test, make sure the path to the script is absolute
+#   let(:script_path) { File.expand_path('../bin/command-line-test', __dir__) }
 
-  context 'with an invalid directory' do
-    let(:chdir_path) { 'does_not_exist' }
+#   context 'with an invalid directory' do
+#     let(:chdir_path) { 'does_not_exist' }
 
-    it 'should raise ERRNO::ENOENT' do
-      expect { subject }.to raise_error(Errno::ENOENT)
-    end
+#     it 'should raise ERRNO::ENOENT' do
+#       expect { subject }.to raise_error(Errno::ENOENT)
+#     end
 
-    context 'on truffleruby', if: truffleruby? do
-      it 'incorrectly returns a non-zero exitstatus without raising an error' do
-        expect(subject).to have_attributes(exitstatus: 1)
-      end
-    end
-  end
+#     context 'on truffleruby', if: truffleruby? do
+#       it 'incorrectly returns a non-zero exitstatus without raising an error' do
+#         expect(subject).to have_attributes(exitstatus: 1)
+#       end
+#     end
+#   end
 
-  context 'with a path to a file' do
-    let(:chdir_path) { 'file1.txt' }
+#   context 'with a path to a file' do
+#     let(:chdir_path) { 'file1.txt' }
 
-    before do
-      File.write(chdir_path, 'contents')
-    end
+#     before do
+#       File.write(chdir_path, 'contents')
+#     end
 
-    it 'should raise Errno::ENOTDIR' do
-      # Annoyingly, the error raised is different on MRI Windows
-      expected_error = windows? ? Errno::EINVAL : Errno::ENOTDIR
-      expect { subject }.to raise_error(expected_error)
-    end
+#     it 'should raise Errno::ENOTDIR' do
+#       # Annoyingly, the error raised is different on MRI Windows
+#       expected_error = windows? ? Errno::EINVAL : Errno::ENOTDIR
+#       expect { subject }.to raise_error(expected_error)
+#     end
 
-    context 'on truffleruby', if: truffleruby? do
-      it 'incorrectly returns a non-zero exitstatus without raising an error' do
-        expect(subject).to have_attributes(exitstatus: 1)
-      end
-    end
-  end
-end
+#     context 'on truffleruby', if: truffleruby? do
+#       it 'incorrectly returns a non-zero exitstatus without raising an error' do
+#         expect(subject).to have_attributes(exitstatus: 1)
+#       end
+#     end
+#   end
+# end
